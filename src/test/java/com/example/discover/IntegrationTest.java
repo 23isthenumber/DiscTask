b827404afaead02da3d88e11310eae3d1bf4681e
Siadul <michal.siadul@capgemini.com>
2022-12-14 12:12:14 +0100

    more reliable integration test

--- src/test/java/com/example/discover/IntegrationTest.java
+++ src/test/java/com/example/discover/IntegrationTest.java
@@ -3,21 +3,32 @@ package com.example.discover;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.*;
 
 class IntegrationTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
     @Test
     void main_validInput_applicationStartsAndWorksCorrectly() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+        String expectedOutput = "4444 4444 4444 4444";
+
         mockInput();
         assertDoesNotThrow(() -> DiscovertaskApplication.main(new String[0]));
+        assertTrue(outContent.toString()
+                             .contains(expectedOutput));
     }
 
     private void mockInput() {
-        String expectedResponse = "4444444444444444";
-        InputStream in = new ByteArrayInputStream(expectedResponse.getBytes());
+        String input = "4444444444444444";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
     }
 }
